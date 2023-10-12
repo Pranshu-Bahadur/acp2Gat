927d9c692b459b3d46dc82e6b4c562929a00b598
root <pranshubahadur@gmail.com>
2023-10-12 13:40:08 +0000

    80.67

--- layer.py
+++ layer.py
@@ -1,6 +1,6 @@
 from keras_core.layers import GroupNormalization
 from tensorflow.keras.layers import Layer, Dense, MultiHeadAttention, \
- LeakyReLU, LayerNormalization
+ LeakyReLU, LayerNormalization, ReLU, Conv2D
 from tensorflow.keras import Sequential
 from itertools import product, repeat, combinations
 import torch
@@ -20,12 +20,14 @@ class GAT(Layer):
         value_dim=2, 
         dropout=0.2), 
         list(range(self.seq_len//self.ngrams))))
-        self.activ = LeakyReLU()
         self.graph_weights = PositionEmbedding(sequence_length=self.seq_len//self.ngrams)
         _num_heads = 4
         self.graph_attention = MultiHeadAttention(key_dim=self.units//_num_heads,
          num_heads=_num_heads, 
          dropout=0.2)
+        self.subgraph_norm = GroupNormalization(self.seq_len//self.ngrams)
+        self.activ = LeakyReLU()
+  
 
     def call(self, x, training=False):
       shape = tf.shape(x)
@@ -33,12 +35,12 @@ class GAT(Layer):
       chunks = tf.split(x, self.seq_len//self.ngrams, axis=2)
       subsets = list(map(lambda subset: combinations(tf.split(subset, self.ngrams, -1), r=2), chunks))
       nodes = list(map(lambda edges: list(map(lambda edge: tf.concat(edge, -1), list(edges))), subsets))
-      alpha = list(map(lambda a, h: self.activ(tf.concat(h, -1) + a(tf.concat(h, -1), tf.concat(h, -1))), self.mhas, nodes))
+      alpha = list(map(lambda a, h: tf.concat(h, -1) + a(tf.concat(h, -1), tf.concat(h, -1)), self.mhas, nodes))
       x = tf.stack(alpha, -2)
-      print(x.shape)
+      x = self.activ(x)
       x = tf.reduce_mean(x, -1)
+      x = self.subgraph_norm(x)
       x = tf.transpose(x, perm=[0, 2, 1])
-      #x = tf.reduce_mean(x, -1)
       x = x + self.graph_weights(x)
       _attention = self.graph_attention(*repeat(x, 2))
       x = x + _attention
