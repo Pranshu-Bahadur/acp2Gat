8d4a414d55c9d80d31a30683b543100f45a245e1
root <pranshubahadur@gmail.com>
2023-10-13 14:08:06 +0000

    81.1%

--- layer.py
+++ layer.py
@@ -1,6 +1,6 @@
 from keras_core.layers import GroupNormalization
 from tensorflow.keras.layers import Layer, Dense, MultiHeadAttention, \
- LeakyReLU, LayerNormalization, ReLU, Conv2D
+ LeakyReLU, LayerNormalization, ReLU, Conv2D, Dropout
 from tensorflow.keras import Sequential
 from itertools import product, repeat, combinations
 import torch
@@ -26,7 +26,14 @@ class GAT(Layer):
          num_heads=_num_heads, 
          dropout=0.2)
         self.subgraph_norm = GroupNormalization(self.seq_len//self.ngrams)
-        self.activ = LeakyReLU()
+        #self.activ = LeakyReLU()
+        self.ffn = Sequential([
+          Dense(self.units),
+          Dense(self.units, activation='gelu'),
+          Dropout(0.2)
+        ])
+        self.attention_norm = LayerNormalization()
+        self.ffn_norm = LayerNormalization()
   
 
     def call(self, x, training=False):
@@ -37,12 +44,14 @@ class GAT(Layer):
       nodes = list(map(lambda edges: list(map(lambda edge: tf.concat(edge, -1), list(edges))), subsets))
       alpha = list(map(lambda a, h: tf.concat(h, -1) + a(tf.concat(h, -1), tf.concat(h, -1)), self.mhas, nodes))
       x = tf.stack(alpha, -2)
-      x = self.activ(x)
       x = tf.reduce_mean(x, -1)
+      #x = self.activ(x)
       x = self.subgraph_norm(x)
       x = tf.transpose(x, perm=[0, 2, 1])
       x = x + self.graph_weights(x)
       _attention = self.graph_attention(*repeat(x, 2))
       x = x + _attention
-      print(x.shape)
+      x = self.attention_norm(x)
+      x = self.ffn(x) + x
+      x = self.ffn_norm(x)
       return x
